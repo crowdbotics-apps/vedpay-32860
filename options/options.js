62a078479748a9108d4b1792394c30d3997e6446
Crowdbotics <team@crowdbotics.com>
2022-01-15 16:50:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "vedpay_32860",
+  url: "https://vedpay_32860.botics.co",
+  api: "https://vedpay_32860.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
